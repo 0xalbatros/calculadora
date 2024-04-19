abfa3b42c187ac2bad03dba7437834df37f4fd82
Alan Bojorquez <aebc95#gmail.com>
2024-04-19 15:23:42 -0700

    operaciones algebraicas

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 import math
 
+print("")
+print("Comandos:")
+print("help")
+print("exit")
+print("")
 
 
 def calculadora():
@@ -7,13 +12,7 @@ def calculadora():
   dic_math = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre[0] != "_"}
   dic_operaciones = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre[0] != "_" and callable(getattr(math, nombre))}
   dic_mathNums = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre == "pi" or nombre == "e" or nombre == "tau" }
-
-
-  print("")
-  print("Comandos:")
-  print("help")
-  print("exit")
-  print("")
+  dic_math.update({"fx":eval})
 
   try:
     operacion = input("Input: ")
@@ -25,11 +24,14 @@ def calculadora():
       print("")
       print("Numeros matematicos:", *dic_mathNums.keys())
       print("")
-      print("Escribe la operacion en una linea. Puedes anidar operaciones utilizando parenstesis (). e.g. Input: ((pi*3)**2)/2")
+      print("Operaciones algebraicas:", "fx")
+      print("")
+      print("Escribe la operacion en una linea. Puedes anidar operaciones utilizando parenstesis (). e.g. Input: ((pi*3)**2)/2.")
       print("Al usar una funcion valida debes utilizar parentesis para escribir la variable. e.g. Input: sin(5), log(3), sqrt(9).")
       print("Los numeros matematicos no requieren parentesis. e.g. Input: pi*3")
       print("Hay algunas funciones validas que requieren dos variables. e.g. pow(2,2).")
-      print("Precionando la flecha hacia arriba en tu teclado, puedes recuperar el input anterior")
+      print("Para obtener el resultado solo preciona Enter.")
+      print("Precionando la flecha hacia arriba en tu teclado, puedes recuperar el input anterior.")
       print("La calculadora utiliza la libreria matematica de python, si quieres mas detalles sobre una funcion los puedes encontrar aqui: https://docs.python.org/3/library/math.html")
       print("")
       return calculadora()
