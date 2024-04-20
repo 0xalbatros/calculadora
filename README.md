67aa466f2f947ac0cd9716553ec2893c44f3a5c2
Alan Bojorquez <aebc95#gmail.com>
2024-04-20 01:22:36 -0700

    documentacion

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # calculadora
+
+Una simple calculadora para utilizar en la consola. Puede realizar todas las operaciones de la libreria math the python y resolver funciones algebraicas.
+
+## Requerimientos
+
+1.Git: Necesitas tener Git instalado en tu sistema para poder clonar el repositorio desde GitHub. Puedes descargar e instalar Git desde el sitio web oficial: https://git-scm.com/.
+2.Python: Debes tener Python instalado en tu sistema para poder ejecutar el archivo de Python. Puedes descargar e instalar Python desde el sitio web oficial: https://www.python.org/. Asegúrate de configurar correctamente la variable de entorno PATH durante la instalación para que puedas ejecutar el comando python desde cualquier ubicación en tu terminal.
+
+## Clonar el repositorio
+
+1.Abre tu terminal y utiliza el comando git clone seguido de la URL del repositorio de GitHub para clonar el repositorio en tu máquina local. Por ejemplo:
+
+```bash
+git clone https://github.com/0xalbatros/calculadora.git
+```
+
+2.Navegar al directorio del repositorio: Utiliza el comando cd para cambiar al directorio del repositorio que acabas de clonar. Por ejemplo:
+
+```bash
+cd calculadora
+```
+
+3.Ejecutar el archivo de Python: Una vez en el directorio del repositorio, ejecuta el archivo de Python utilizando el intérprete de Python. Por ejemplo, si el archivo se llama script.py, puedes ejecutarlo con el siguiente comando:
+
+```bash
+python calculadora.py
+```
+
+## Uso
+
+1. Abre la consola y navega hasta la ubicación del archivo `calculadora.py`.
+2. Ejecuta el siguiente comando para iniciar la calculadora:
+
+```bash
+python calculadora.py
+```
+
+Asegurate de escribir el comando desde la ubicacion del archivo.
+
+Si se ejecuto correctamente te aparecera algo asi:
+
+```bash
+Comandos:
+operacion
+algebra
+funciones
+constantes
+exit
+
+
+Input:
+```
+
+Ahora solo tienes que ingresar las operaciones que desees y presionar enter para obtener el resultado.
+
+Ejemplo:
+
+```bash
+Input: (pi*3)*cos(4)/sin(2)
+```
+
+La calculadora tiene una lista de comandos que puedes utilizar para obtener mayor informacion de como utilizar cada funcion.
+
+Puedes realizar operaciones algebraicas y asignarle valores a variables
+
+Ejemplo:
+
+```bash
+Input: fn('x**2+2*y+5', {'x':3, 'y':5})
+```
+Reutilizar los valores de 'x' y 'y'
+
+```bash
+Input: 8*x + y**2
+```
+Donde conservaran los valores asignados en el input anterior
+
+Puedes ver mas detalles de uso con con el menu de comandos, solo escribe lo que te interese ver.
+
+Ejemplo:
+
+```bash
+Comandos:
+operacion
+algebra
+funciones
+constantes
+exit
+
+
+Input:funciones
+```
+
+Para salir del archivo solo escribe exit
+
+```bash
+Input: exit
+```
+
+La consola es sensible a los espacios asi que asegurate de siempre escribir exactamente el comando que deseas sin espacios adicionales, y respetando comillas, parentesis, letras, etc.
+
+Gracias por utilizar la calculadora. 
--- calculadora.py
+++ calculadora.py
@@ -1,46 +1,47 @@
 import math
 
-print("")
-print("Comandos:")
-print("help")
-print("exit")
-print("")
+dic_operaciones = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre[0] != "_" and callable(getattr(math, nombre))}
+dic_mathNums = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre == "pi" or nombre == "e" or nombre == "tau" }
+dic_operaciones.update(dic_mathNums)
 
+def algebra(expresion, variables):
+  try:
+    if any(len(k) > 1 or k == "e" for k in variables):
+      raise Exception(f"{variables} invalido")
+    dic_operaciones.update(variables)
+    return eval(expresion, dic_operaciones)
+  except Exception as error:
+    print(error)
+
+dic_operaciones.update({"fn":algebra})
+
+print("\n\nComandos:\noperacion\nalgebra\nfunciones\nconstantes\nexit\n\n")
 
 def calculadora():
-
-  dic_math = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre[0] != "_"}
-  dic_operaciones = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre[0] != "_" and callable(getattr(math, nombre))}
-  dic_mathNums = {nombre: getattr(math, nombre) for nombre in dir(math) if nombre == "pi" or nombre == "e" or nombre == "tau" }
-  dic_math.update({"fx":eval})
-
   try:
     operacion = input("Input: ")
     if operacion == "exit":
       return print("Fin del programa")
-    if operacion == "help":
-      print("")
-      print("Funciones validas:", *dic_operaciones.keys())
-      print("")
-      print("Numeros matematicos:", *dic_mathNums.keys())
-      print("")
-      print("Operaciones algebraicas:", "fx")
-      print("")
-      print("Escribe la operacion en una linea. Puedes anidar operaciones utilizando parenstesis (). e.g. Input: ((pi*3)**2)/2.")
-      print("Al usar una funcion valida debes utilizar parentesis para escribir la variable. e.g. Input: sin(5), log(3), sqrt(9).")
-      print("Los numeros matematicos no requieren parentesis. e.g. Input: pi*3")
-      print("Hay algunas funciones validas que requieren dos variables. e.g. pow(2,2).")
-      print("Para obtener el resultado solo preciona Enter.")
-      print("Precionando la flecha hacia arriba en tu teclado, puedes recuperar el input anterior.")
-      print("La calculadora utiliza la libreria matematica de python, si quieres mas detalles sobre una funcion los puedes encontrar aqui: https://docs.python.org/3/library/math.html")
-      print("")
+    if operacion == "operacion":
+      print("\n\nInstrucciones:\n\n>>Escribe la operacion en una linea. Puedes anidar operaciones utilizando parenstesis (). \n\n\tEjemplo:\n\tInput: ((pi*3)**2)/2. \n\n>>Para obtener el resultado presiona Enter. \n\n>>Para obtener el input anterior presiona flecha arriba\n\n")
       return calculadora()
-    resultado = eval(operacion, dic_math)
-    print(f"Resultado: {resultado}")
-    print("")
+    if operacion == "algebra":
+      print("\n\nInstrucciones:\n\n>>Las operaciones algebraicas deben estar escritas de la siguiente manera fn('', {'x':1}). \n\n\tEjemplo:\n\tInput: fn('x**2', {'x':5}).\n\n>>Puedes introducir mas de una variable. \n\n\tEjemplo:\n\tInput: fx('x**2+y', {'x':5, 'y':3}).\n\n>>Puedes declarar valores de variables y posteriormente usarlos en una operacion sin necesidad de escribir fn() y utilizar comillas. \n\n\tEjemplo:\n\tDeclaras el valor de x \n\tInput: fn('x', {'x':5})\n\tluego utilizar x en una operacion \n\tInput: x*5\n\tEl valor de x se conserva hasta que se cierre la aplicacion o hasta que se le reasigne valor.\n\n>>Para casos donde se multilpica la variable es necesario indicar el *.\n\n\tEjemplo:\n\t'5x' es invalido '5*x es correcto'\n\n>>Cuando utilizas fn(), es importante escribir la expresión entre comillas 'x+3' y las variables entre llaves y con comillas {'x':5, 'y':3, 'z':7}\n\n")
+      return calculadora()
+    if operacion == "funciones":
+      print("\nFunciones validas:", *dic_operaciones.keys())
+      print("\n\nInstrucciones:\n\n>>Al usar una funcion valida debes utilizar parentesis para escribir la variable. \n\n\tEjemplo:\n\tInput: sin(5), log(3), sqrt(9).\n\n>>Hay algunas funciones validas que requieren dos variables. \n\n\tEjemplo:\n\tpow(2,2).\n\n>>La calculadora utiliza la libreria matematica de python, si quieres mas detalles sobre una funcion los puedes encontrar aqui: https://docs.python.org/3/library/math.html \n\n")
+      return calculadora()
+    if operacion == "constantes":
+      print("\nNumeros matematicos:", *dic_mathNums.keys())
+      print("\n\nInstrucciones:\n\n>>Se utilizan como cualquier otro numero, no requieren parentesis. \n\n\tEjemplo:\n\tInput: pi*3/e\n\n")
+      return calculadora()
+    resultado = eval(operacion, dic_operaciones)
+    print(f"Resultado: {resultado}\n\n")
     return calculadora()
   except Exception as error:
     print("Error:",error)
     return calculadora()
 
 calculadora()
+
